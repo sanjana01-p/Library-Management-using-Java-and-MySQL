fe0b517fcbd4704206c9060c25d66842d1ff9493
Deepak0113 <deepakmugundan13@gmail.com>
2024-07-13 22:17:03 +0530

    adding

---
+++ src/data/Book.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Book {
+    private String name;
+    private String id;
+    private int copies;
+    private String author;
+
+    public Book(String name, String id, int copies, String author) {
+        this.name = name;
+        this.id = id;
+        this.copies = copies;
+        this.author = author;
+    }
+
+    public void showBook() {
+        System.out.println("Name: " + name);
+        System.out.println("Id: " + id);
+        System.out.println("Copies available: " + copies);
+        System.out.println("Author: " + author);
+    }
+}
